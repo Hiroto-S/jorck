d2e17f65f4f052313eb5fc4dde04512e3070c342
hiroto <hiro10.seabass@gmail.com>
2019-11-07 15:29:10 +0900

    comment :first commit

---
+++ lib/jorck.rb
@@ -0,0 +1,8 @@
+require "jorck/version"
+
+module Jorck
+  class Error < StandardError; end
+  def self.hello_jorck
+    p "Hello, jorck"
+  end
+end
---
+++ lib/jorck/version.rb
@@ -0,0 +1,3 @@
+module Jorck
+  VERSION = "0.1.0"
+end
